d2f334606c0a9bcfb9c5339e7e4589966cbf29f1
breadoven <56191411+breadoven@users.noreply.github.com>
2020-10-13 13:56:32 +0100

    OSD Nav Messages Update
    RTH climb message added.
    WP hold time countdown and To WP number countdown added to system messages.

--- src/main/io/osd.c
+++ src/main/io/osd.c
@@ -786,20 +786,20 @@ static const char * navigationStateMessage(void)
         case MW_NAV_STATE_NONE:
             break;
         case MW_NAV_STATE_RTH_START:
-            return OSD_MESSAGE_STR("STARTING RTH");
+            return OSD_MESSAGE_STR("STARTING RTH");    
+        case MW_NAV_STATE_RTH_CLIMB:            
+            return OSD_MESSAGE_STR("ADJUSTING RTH ALTITUDE");            
         case MW_NAV_STATE_RTH_ENROUTE:
-            // TODO: Break this up between climb and head home
             return OSD_MESSAGE_STR("EN ROUTE TO HOME");
         case MW_NAV_STATE_HOLD_INFINIT:
             // Used by HOLD flight modes. No information to add.
             break;
-        case MW_NAV_STATE_HOLD_TIMED:
-            // TODO: Maybe we can display a count down
-            return OSD_MESSAGE_STR("HOLDING WAYPOINT");
+        case MW_NAV_STATE_HOLD_TIMED:            
+            // "HOLDING WP FOR xx S" Countdown added in osdDrawSingleElement
             break;
         case MW_NAV_STATE_WP_ENROUTE:
-            // TODO: Show WP number
-            return OSD_MESSAGE_STR("TO WP");
+            // "TO WP" + WP countdown added in osdDrawSingleElement
+            break;
         case MW_NAV_STATE_PROCESS_NEXT:
             return OSD_MESSAGE_STR("PREPARING FOR NEXT WAYPOINT");
         case MW_NAV_STATE_DO_JUMP:
@@ -2071,10 +2071,24 @@ static bool osdDrawSingleElement(uint8_t item)
                         // will cause it to be missing from some frames.
                     }
                 } else {
-                    if (FLIGHT_MODE(NAV_RTH_MODE) || FLIGHT_MODE(NAV_WP_MODE) || navigationIsExecutingAnEmergencyLanding()) {
-                        const char *navStateMessage = navigationStateMessage();
-                        if (navStateMessage) {
-                            messages[messageCount++] = navStateMessage;
+                    if (FLIGHT_MODE(NAV_RTH_MODE) || FLIGHT_MODE(NAV_WP_MODE) || navigationIsExecutingAnEmergencyLanding()) {                        
+                        if (NAV_Status.state == MW_NAV_STATE_WP_ENROUTE) {
+                            // Countdown display for remaining Waypoints
+                            tfp_sprintf(messageBuf, "TO WP %u/%u", posControl.activeWaypointIndex + 1, posControl.waypointCount);                            
+                            messages[messageCount++] = messageBuf;
+                        } else if (NAV_Status.state == MW_NAV_STATE_HOLD_TIMED) {
+                            // WP hold time countdown in seconds
+                            timeMs_t currentTime = millis();
+                            int holdTimeRemaining = posControl.waypointList[posControl.activeWaypointIndex].p1 - (int)((currentTime - posControl.wpReachedTime)/1000);
+                            if (holdTimeRemaining >=0) {
+                              tfp_sprintf(messageBuf, "HOLDING WP FOR %2u S", holdTimeRemaining);
+                              messages[messageCount++] = messageBuf;
+                            }
+                        } else {                            
+                            const char *navStateMessage = navigationStateMessage();                             
+                            if (navStateMessage) {
+                                messages[messageCount++] = navStateMessage;
+                            }
                         }
                     } else if (STATE(FIXED_WING_LEGACY) && (navGetCurrentStateFlags() & NAV_CTL_LAUNCH)) {
                             messages[messageCount++] = "AUTOLAUNCH";
