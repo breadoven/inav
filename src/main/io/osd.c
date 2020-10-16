e18768a07f398ad2bef3625d2d4998767a90ea90
breadoven <56191411+breadoven@users.noreply.github.com>
2020-10-16 11:40:11 +0100

    Revert "Update osd.c"
    This reverts commit acf32545b61bc2e2db7ed24a5205f7e238d12ffc.

--- src/main/io/osd.c
+++ src/main/io/osd.c
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
  */
-// test
+
 /*
  Created by Marcin Baliniak
  some functions based on MinimOSD
